9e22b80b02fed687539fe22e50cc080b77283b35
fadhluibnu <f.ibnu.a1@gmail.com>
2021-11-28 13:54:37 +0700

    Versi 1.2

---
+++ script.js
@@ -0,0 +1,6 @@
+var dataSpyList = [].slice.call(
+  document.querySelectorAll('data-bs-spy="scroll"')
+);
+dataSpyList.forEach(function (dataSpyEl) {
+  bootstrap.ScrollSpy.getInstance(dataSpyEl).refresh();
+});

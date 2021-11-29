a4a8bda43baaf528ad02e7a17da5a743a3f46849
fadhluibnu <f.ibnu.a1@gmail.com>
2021-11-29 08:01:34 +0700

    projects

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-var dataSpyList = [].slice.call(
-  document.querySelectorAll('data-bs-spy="scroll"')
-);
-dataSpyList.forEach(function (dataSpyEl) {
-  bootstrap.ScrollSpy.getInstance(dataSpyEl).refresh();
+// var dataSpyList = [].slice.call(
+//   document.querySelectorAll('data-bs-spy="scroll"')
+// );
+// dataSpyList.forEach(function (dataSpyEl) {
+//   bootstrap.ScrollSpy.getInstance(dataSpyEl).refresh();
+// });
+
+$(window).scroll(function () {
+  var wScroll = $(this).scrollTop();
+
+  if (wScroll > $("#home").offset().top - 200) {
+    $("#home-link").addClass("active");
+    $("#projects-link").removeClass("active");
+    $("#skills-link").removeClass("active");
+    $("#contact-link").removeClass("active");
+  }
+  if (
+    wScroll > $("#projects").offset().top - 200 &&
+    wScroll < $("#skills").offset().top
+  ) {
+    $("#home-link").removeClass("active");
+    $("#projects-link").addClass("active");
+    $("#skills-link").removeClass("active");
+    $("#contact-link").removeClass("active");
+  }
+  if (wScroll > $("#skills").offset().top - 200) {
+    $("#home-link").removeClass("active");
+    $("#projects-link").removeClass("active");
+    $("#skills-link").addClass("active");
+    $("#contact-link").removeClass("active");
+  }
+  if (wScroll > $("#contact").offset().top - 200) {
+    $("#home-link").removeClass("active");
+    $("#projects-link").removeClass("active");
+    $("#skills-link").removeClass("active");
+    $("#contact-link").addClass("active");
+  }
 });

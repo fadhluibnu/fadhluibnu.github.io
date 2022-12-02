b8b01e66f59caad4d49ad3d8903a53d1c1cd58fa
Fadhlu Ibnu <f.ibnu.a1@gmail.com>
2022-12-03 03:23:40 +0700

    section 2

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-document.getElementById('button-collapse').addEventListener('click', function(){
+$('#content').scroll(function () {
+    var wScroll = $(this).scrollTop();
+    if (wScroll > 5 ) {
+        document.getElementById('nav-navbar').classList.add('bg-navbar');
+    }else{
+        document.getElementById('nav-navbar').classList.remove('bg-navbar');
+    }
+});
+document.getElementById('button-collapse').addEventListener('click', function () {
     if (document.getElementById('nav-navbar').classList.contains('bg-navbar')) {
         document.getElementById('nav-navbar').classList.remove('bg-navbar');
-    }else{
-    document.getElementById('nav-navbar').classList.add('bg-navbar');
-}
+    } else {
+        document.getElementById('nav-navbar').classList.add('bg-navbar');
+    }
 })
